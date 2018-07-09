805258c5f05fcb5c4fc1871cc25205b2ad506cc4
iAmAnoob2020 <40950311+iAmAnoob2020@users.noreply.github.com>
2018-07-09 17:27:15 +0100

    Add files via upload

--- commands/level.js
+++ commands/level.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
-let xp = require("./xp.json");
+const config = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
+
+  let xp = require("../xp.json");
 
   if(!xp[message.author.id]){
     xp[message.author.id] = {
@@ -14,15 +15,17 @@ module.exports.run = async (bot, message, args) => {
 
 let curxp = xp[message.author.id].xp;
 let curlvl = xp[message.author.id].level;
+let curpres = xp[message.author.id].prestige;
 let nxtLvlXp = curlvl * 300;
 let difference = nxtLvlXp - curxp;
 
 let lvlEmbed = new Discord.RichEmbed()
-.setAuthor(message.author.username)
+.setAuthor(message.author.username + `You leveled up!`)
 .setColor("#ff0000")
 .addField("Level", curlvl,true)
 .addField("XP", curxp,true)
-.addFooter(`${difference} XP till next level up!`, message.author.displayAvatarURL);
+.addField("Prestige", curpres,true)
+.setFooter(`${difference} XP till next level up!`, message.author.displayAvatarURL);
 
 message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
 
